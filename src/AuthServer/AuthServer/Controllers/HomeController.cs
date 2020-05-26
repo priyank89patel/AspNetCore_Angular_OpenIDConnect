76cefdc560b65bd0f83b646eb539ac4b25cfe33d
Priyank Patel <priyank89patel@gmail.com>
2020-05-25 21:22:54 -0400

    Auth Server account and consent controller changes

--- src/AuthServer/AuthServer/Controllers/HomeController.cs
+++ src/AuthServer/AuthServer/Controllers/HomeController.cs
@@ -1,37 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using AuthServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Hosting;
+
 
 namespace AuthServer.Controllers
 {
+    [SecurityHeaders]
+    [AllowAnonymous]
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _environment;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IWebHostEnvironment environment, ILogger<HomeController> logger)
         {
+            _environment = environment;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            if (_environment.IsDevelopment())
+            {
+                // only show in development
+                return View();
+            }
+
+            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
+            return NotFound();
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
