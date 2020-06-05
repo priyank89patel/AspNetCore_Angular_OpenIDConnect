b6745f802724fc9a515779ecf422d119e646f341
Priyank Patel <priyank89patel@gmail.com>
2020-06-04 21:20:18 -0400

    Client config changes

--- src/AuthServer/AuthServer/Config.cs
+++ src/AuthServer/AuthServer/Config.cs
@@ -33,16 +33,18 @@ namespace AuthServer
         {
             return new[]
             {
-                new Client {
+                new Client
+                {
                     RequireConsent = false,
                     ClientId = "angular_spa",
                     ClientName = "Angular SPA",
-                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce=true,
+                    RequireClientSecret=false,
                     AllowedScopes = { "openid", "profile", "email", "api.read" },
-                    RedirectUris = {"http://localhost:4200/auth-callback"},
-                    PostLogoutRedirectUris = {"http://localhost:4200/"},
+                    RedirectUris ={"http://localhost:4200/auth-callback"},
+                    PostLogoutRedirectUris ={"http://localhost:4200"},
                     AllowedCorsOrigins = {"http://localhost:4200"},
-                    AllowAccessTokensViaBrowser = true,
                     AccessTokenLifetime = 3600
                 }
             };
