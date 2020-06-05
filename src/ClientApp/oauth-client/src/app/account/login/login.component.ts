1150ea020eaf7d02ddff0185f67e6afdd3da3ac3
Priyank Patel <priyank89patel@gmail.com>
2020-06-04 21:21:08 -0400

    login and auth callback changes

--- src/ClientApp/oauth-client/src/app/account/login/login.component.ts
+++ src/ClientApp/oauth-client/src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/authentication/auth.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private spinner: NgxSpinnerService) { }
+title="Login";
 
+login(){
+  this.spinner.show();
+  this.authService.login();
+}
   ngOnInit(): void {
   }
 
--- src/ClientApp/oauth-client/src/app/auth-callback/auth-callback.component.ts
+++ src/ClientApp/oauth-client/src/app/auth-callback/auth-callback.component.ts
@@ -14,10 +14,10 @@ export class AuthCallbackComponent implements OnInit {
 
   async ngOnInit() {
     // check for error
-    if (this.route.snapshot.fragment.indexOf('error') >= 0) {
-      this.error = true;
-      return;
-    }
+    // if (this.route.snapshot.fragment.indexOf('error') >= 0) {
+    //   this.error = true;
+    //   return;
+    // }
     await this.authService.completeAuthentication();
     this.router.navigate(['/home']);
   }
